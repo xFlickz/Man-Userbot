92e295754a0bfa45ad20144fcfd09a1b0b2ee474
XFlickz-kunn <95107186+xFlickz@users.noreply.github.com>
2022-01-28 00:14:53 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -54,7 +54,7 @@ async def man_userbot_on():
         if BOTLOG_CHATID != 0:
             await bot.send_message(
                 BOTLOG_CHATID,
-                f"🔥 **Man-Userbot Berhasil Di Aktifkan**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{branch}`\n➠ **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n━━",
+                f"🔥 **Man-Userbot Berhasil Di Kontolkan**\n━━\n➠ **Userbot Version -** `{BOT_VER}@{branch}`\n➠ **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n━━",
             )
     except Exception as e:
         LOGS.info(str(e))
